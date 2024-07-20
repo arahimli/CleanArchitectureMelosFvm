53c051696cbd8b0c5d3b26fd89b6c95ae436f7a5
Rahimli <62797403+arahimli@users.noreply.github.com>
2024-07-20 23:20:28 +0400

    Home page completed

--- apps/news_admin_app/lib/main.dart
+++ apps/news_admin_app/lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Admin app timer:',
+              'Admin app timer: v2aaaaaa',
             ),
             Text(
               '$_counter',
